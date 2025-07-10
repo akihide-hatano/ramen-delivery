071dc936b509d5a61a8957922801285c21a91852
akihide-hatano <a.h.19841202@gmail.com>
2025-07-10 10:20:33 +0900

    2025_07_10_011924_create_cart_items_tableの作成

---
+++ database/migrations/2025_07_10_011924_create_cart_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            // id <BIGSERIAL, NOT NULL, PK>
+            $table->id();
+
+            $table->foreignId('cart_id')->constrained('carts')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+
+            // quantity
+            $table->integer('quantity');
+
+            // 同じカート内で同じ商品が複数回追加されることを防ぎ、数量を更新する仕組みにする場合
+            // このユニーク制約を追加することで、同じ商品がカートに複数行登録されるのを防ぎます。
+            // アプリケーション側で、既に商品が存在する場合はquantityを更新するロジックが必要になります。
+            $table->unique(['cart_id', 'product_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
