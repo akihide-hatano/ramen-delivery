002fbb0aaf0d8f0390decce4cfcf419a5e20af94
akihide-hatano <a.h.19841202@gmail.com>
2025-07-12 12:08:52 +0900

    ShopControllerをindexで作成しました。

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Shop; // Shopモデルをuseする
 
 class ShopController extends Controller
 {
-    //
+    /**
+     * 店舗一覧ページを表示
+     *
+     * @param Request $request
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $shops = Shop::all();
+        dump($shops);
+        // 取得した店舗データを 'shops.index' ビューに渡します。
+        return view('shops.index', compact('shops'));
+    }
 }
