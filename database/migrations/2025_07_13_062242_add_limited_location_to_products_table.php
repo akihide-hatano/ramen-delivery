0cb2a1937d3bd69d76f23a5ed15455b2a9aeb753
akihide-hatano <a.h.19841202@gmail.com>
2025-07-13 15:27:37 +0900

    2025_07_13_062242_add_limited_location_to_products_tableを作成

---
+++ database/migrations/2025_07_13_062242_add_limited_location_to_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // limited_location カラムを追加。null許容。
+            $table->string('limited_location')->nullable()->after('is_limited');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('limited_location');
+        });
+    }
+};
